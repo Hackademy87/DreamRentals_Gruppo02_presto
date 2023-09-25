5db02a537affbd278f3282f6cb61ea6600579c54
Andrea Di Virgilio <andrea2003dv@gmail.com>
2023-09-25 16:22:58 +0200

    CssLogin

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,8 +1,8 @@
 <x-layout>
 
 
-    <video class="video-bg " src="pittura.mp4" autoplay muted loop></video>
-<div class="container my-5">
+  <video class="video-bg " src="pittura.mp4" autoplay muted loop></video>
+  <div class="container my-5">
     <div class="row justify-content-center align-items-center">
 
 
@@ -10,36 +10,36 @@
 
 
 
-<form class="form-container" action="{{route('login')}}" method="POST">
-  @csrf
-      <div class="logo-container">
-        Welcome Back!
-      </div>
-
-
-      <div class="line"></div>
-      <form class="form">
-        <div class="form-group">
-          <label for="email">Email</label>
-          <input required="" placeholder="Enter your email" name="email" id="email" type="email" value="{{old('email')}}">
+      <form class="form-container" action="{{route('login')}}" method="POST">
+        @csrf
+        <div class="logo-container">
+          Welcome Back!
         </div>
 
-        <div class="form-group">
-          <label for="password">Password</label>
-          <input required="" name="password" placeholder="Enter your password" id="password" type="password" value="{{old('password')}}">
-        </div>
 
-        <button type="submit" class="form-submit-btn">Sign In</button>
-      <p class="signup-link">
-        Don't have an account?
-        <a class="signup-link link" href="{{route('register')}}"> Sign up now</a>
-      </p>
-</form>
+        <div class="line"></div>
+        <form class="form">
+          <div class="form-group">
+            <label for="email">Email</label>
+            <input required="" placeholder="Enter your email" name="email" id="email" type="email" value="{{old('email')}}">
+          </div>
+
+          <div class="form-group">
+            <label for="password">Password</label>
+            <input required="" name="password" placeholder="Enter your password" id="password" type="password" value="{{old('password')}}">
+          </div>
+
+          <button type="submit" class="form-submit-btn">Sign In</button>
+          <p class="signup-link">
+          Don't have an account?
+          <a class="signup-link link" href="{{route('register')}}"> Sign up now</a>
+          </p>
+        </form>
 
 
 
-</div>
-</div>
+    </div>
+  </div>
 
 <!-- Login importato -->
 
