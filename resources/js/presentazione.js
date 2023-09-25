7bafdb131de7486f649a3436c179a54aef91e3fd
Andrea Di Virgilio <andrea2003dv@gmail.com>
2023-09-25 19:33:17 +0200

    SS

--- resources/js/presentazione.js
+++ resources/js/presentazione.js
@@ -1,17 +1,21 @@
     //? PRESENTAZIONE
 
     const text = document.querySelector(".sec-text");
-    const textLoad = () => {
-      setTimeout(() => {
-        lollo.textContent = "The Difference";
-      }, 0);
-      setTimeout(() => {
-        lollo.textContent = "Your Future";
-      }, 4000);
-      setTimeout(() => {
-        lollo.textContent = "Your Destination";
-      }, 8000);
-    }
-
-    textLoad();
-    setInterval(textLoad, 4000);
+    if(text){
+      const textLoad = () => {
+        setTimeout(() => {
+          lollo.textContent = "The Difference";
+        }, 0);
+        setTimeout(() => {
+          lollo.textContent = "Your Future";
+        }, 4000);
+        setTimeout(() => {
+          lollo.textContent = "Your Destination";
+        }, 8000);
+      }
+  
+      textLoad();
+      setInterval(textLoad, 4000);
+    }     
+    
+    
